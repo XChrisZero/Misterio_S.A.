19e84a420fa4b703ff1ff6f552b25b1a56469295
Christian Kuriak Chung Ferrari <161791400+XChrisZero@users.noreply.github.com>
2024-09-01 17:41:33 -0300

    Update Humano.java

--- Humano.java
+++ Humano.java
@@ -1,20 +1,32 @@
+package JAV.Scooby_Doo.Misterios_SA;
+
 public class Humano 
 {
     String nome;
     int idade;
     float peso;
-    String cor;
+    String frase;
+    String olho;
+    String cabelo;
 
-    void dadosh()
+    void dadosH()
     {
-        System.out.print("Cor: ");
-        System.out.println(cor);
+        System.out.print("frase: ");
+        System.out.println(frase);
         System.out.print("nome: ");
         System.out.println(nome);
         System.out.print("idade: ");
         System.out.println(idade);
         System.out.print("peso: ");
         System.out.println(peso);
+        System.out.print("olho: ");
+        System.out.println(olho);
+        System.out.print("cabelo: ");
+        System.out.println(cabelo);
         System.out.println("------------");
     }   
+    void Jargao()
+    {
+        System.out.println("Que tal uns biscoitos scooby?");
+    }
 }
